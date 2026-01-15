944d428a654d966dd0b4c3dec7a8bf3b05ca993c
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-15 19:40:33 +0600

    feat:basic file ready

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+let app = express()
+let port = process.env.PORT || 4000;
+
+app.get("/" , (req,res) =>{
+    res.send("Hello");
+});
+
+
+app.listen( port , () =>{
+    console.log(`server stated at ${port} `);
+    
+})
