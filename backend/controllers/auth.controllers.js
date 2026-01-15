63062fca907594d17d911e64e1f84e47b93aa350
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-15 22:19:00 +0600

    hashPassword with bcryptjs

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,5 @@
 import User from "../model/user.model.js";
-
+import bcrypt from "bcryptjs"
 export const signUp = async (req,res) =>{
     try {
         const {firstname,lastname,userName,email,password} = req.body;
@@ -9,6 +9,15 @@ export const signUp = async (req,res) =>{
         if (existUser) {
             return res.status(400).json({message:"user already exist"})
         }
+
+        // ========= 3rd Step === hash Password ======
+
+        const hashPassword = await bcrypt.hash(password,15)
+
+
+
+
+
     } catch (error) {
         return res.status(500).json({message:"Intarnel server Error"});
     }
