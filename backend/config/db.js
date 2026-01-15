13376d3c2d2192d511dc7452e7d944750449da65
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-15 21:11:28 +0600

    feat:successfuly connected DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const connectDB =  async () =>{
+ const connectDB =  async () =>{
     try {
         await mongoose.connect(process.env.MONGODB);
+        console.log("DB Connected");
+        
     } catch (error) {
         console.log("Database Erorr :" , error.message);
         
     }
 }
+export default connectDB
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
-
+import connectDB from "./config/db.js";
 let app = express();
 let port = process.env.PORT || 4000;
 
@@ -13,6 +13,7 @@ app.get("/" , (req,res) =>{
 
 
 app.listen( port , () =>{
+    connectDB()
     console.log(`server stated at ${port} `);
     
 });
