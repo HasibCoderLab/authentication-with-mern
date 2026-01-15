d101b927ea9283491a6bbababdd0a2ed3b037e0e
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-15 22:15:11 +0600

    check user in DB

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,6 +1,14 @@
+import User from "../model/user.model.js";
+
 export const signUp = async (req,res) =>{
     try {
-        const {firstname,lastname,userName,email,password} = req.body
+        const {firstname,lastname,userName,email,password} = req.body;
+
+        // ========= 2nd Step === check have a user in DB ======
+        let existUser = await User.findOne({email});
+        if (existUser) {
+            return res.status(400).json({message:"user already exist"})
+        }
     } catch (error) {
         return res.status(500).json({message:"Intarnel server Error"});
     }
