0e4b85bf04ef804cde0f59d83da90c8e0a3dd87b
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-16 17:08:00 +0600

    add element

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <Routes>
-      <Route/>
-      <Route/>
+      <Route path='signup' element={<SignUp />}/>
+      <Route path='login' element={<Login />}/>
 
     </Routes>
   )
