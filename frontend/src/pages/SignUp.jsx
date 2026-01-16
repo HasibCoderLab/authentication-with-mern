d421ed45cc4fcfb390f8f5b67e29ff25271890cf
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-16 19:17:25 +0600

    include img

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -5,12 +5,23 @@ const SignUp = () => {
         <div className='w-full h-screen bg-black flex items-center justify-center'>
             <div className='w-[90%] max-w-125 h-125 bg-[#141f1f] rounded flex flex-col justify-center items-center gap-5'>
                 <h1 className="text-white text-[20px] font-semibold "> Sign Up</h1>
+                {/* ============= Form  =============  */}
                 <form className=' w-full flex flex-col justify-center items-center gap-5'>
-                    <div>
-                        <input type="text" placeholder='First  Name' />
-                        <input type="text" placeholder='Last  Name' />
+                    <div className='w-[80%] h-12.5 flex  justify-center items-center gap-2.5'>
 
-                    </div>
+                        {/* ======================  Input field ========== */}
+
+
+                        <input type="text" placeholder='First  Name' className='w-[50%] h-full bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
+                        <input type="text" placeholder='Last  Name' className='w-[50%] h-full bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
+                        {/* ======================  Input field ========== */}
+</div>
+                        <input type="text" placeholder='UserName' className='w-[80%] l h-[50%] bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
+
+                        <input type="email" placeholder='Enter Email' className='w-[80%]  h-[50%] bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
+
+                        <input type="password" placeholder='Enter Password' className='w-[80%] h-[50%] bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
+                    
                 </form>
             </div>
 
