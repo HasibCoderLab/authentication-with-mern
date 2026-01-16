0395cf6677c4c11afe7d2bf37e7b0b712fb5622a
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-16 20:17:06 +0600

    simple done Sign Up

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -12,7 +12,7 @@ const SignUp = () => {
 
                     <div className="w-25 h-25 rounded-full bg-white relative ">
                         <img src={dp} alt="database Picture" className="w-full h-full rounded-full " />
-                        <div className="absolute w-full h-full bg-black top-0 opacity-0 hover:opacity-30 flex items-center justify-center text-white text-[40px] font-semibold cursor-pointer ">
+                        <div className="absolute w-full h-full bg-black rounded-full top-0 opacity-0 hover:opacity-30 flex items-center justify-center text-white text-[40px] font-semibold cursor-pointer ">
                             +
                         </div>
                     </div>
@@ -22,22 +22,22 @@ const SignUp = () => {
                         {/* ======================  Input field ========== */}
 
                         <input type="text" placeholder='First  Name'
-                         className='w-[50%] h-full bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
+                            className='w-[50%] h-full bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
 
-                        <input type="text" placeholder='Last  Name' 
-                        className='w-[50%] h-full bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
+                        <input type="text" placeholder='Last  Name'
+                            className='w-[50%] h-full bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
 
                         {/* ======================  Input field email,pass,userName ========== */}
                     </div>
-                    <input type="text" placeholder='UserName' 
-                    className='w-[80%] l h-[50%] bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
+                    <input type="text" placeholder='UserName'
+                        className='w-[80%] l h-[50%] bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
 
-                    <input type="email" placeholder='Enter Email' 
-                    className='w-[80%]  h-[50%] bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
+                    <input type="email" placeholder='Enter Email'
+                        className='w-[80%]  h-[50%] bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
 
                     <input type="password" placeholder='Enter Password'
-                     className='w-[80%] h-[50%] bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
-
+                        className='w-[80%] h-[50%] bg-white outline-none border-none rounded-lg px-2.5 py-1.5' />
+                    <button className="bg-[#07c7e4] text-black px-2.5 py-1.5 rounded-lg">Login </button>
                 </form>
             </div>
 
