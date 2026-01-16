954f97841f07df3eddf71c32efec38c7303e1d09
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-16 17:33:34 +0600

    basic style

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
 const SignUp = () => {
-  return (
-    <div className='w-full h-[100vh] bg-black flex items-center justify-center'>
-        <div className='w-[90%] max-w-[500px] h-[500px] bg-[#14afaf]'></div>
+    return (
+        <div className='w-full h-screen bg-black flex items-center justify-center'>
+            <div className='w-[90%] max-w-125 h-125 bg-[#141f1f] rounded flex flex-col justify-center items-center gap-5'>
+                <h1 className="text-white text-[20px] font-semibold "> Sign Up</h1>
+                <form className=' w-full flex flex-col justify-center items-center gap-5'>
+                    <div>
+                        <input type="text" placeholder='First  Name' />
+                        <input type="text" placeholder='Last  Name' />
 
-    </div>
-  )
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    )
 }
 
 export default SignUp
