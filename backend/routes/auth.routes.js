e6b954966f83016cc62f88a472a3c65f64119c2c
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-17 11:28:12 +0600

    add route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,8 @@
 
 import express, { Router }  from "express";
-import { login, logout, signUp } from "../controllers/auth.controllers.js";
+import { getUserData, login, logout, signUp } from "../controllers/auth.controllers.js";
 import { upload } from "../middlewares/multer.js";
+import { checkAuth } from "../middlewares/checkAuth.js";
 
 
 const authRouter = express(Router());
@@ -9,6 +10,7 @@ const authRouter = express(Router());
 authRouter.post("/signup", upload.single("uploadImage"),signUp);
 authRouter.post("/login",login);
 authRouter.post("/logout",logout);
+authRouter.get("/getUserData", checkAuth ,getUserData);
 
 
 
