199f07cc8b8c3532a755a22764798c9b8e631c99
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-17 11:26:08 +0600

    getUser [api]

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -149,3 +149,23 @@ export const logout = async (req, res) => {
     }
 
 } 
+
+
+// ============ Get User [Api] =======
+export const getUserData =  async(req,res) =>{
+
+    try {
+        let userId = req.userId;
+        if (!userId) {
+            return res.status(400).json({message:"user id is not found"});
+        }
+        let user = await User.findById({userId});
+        if (!user) {
+            return res.status(400).json({message:"user id is not found"});            
+        }
+            return res.status(200).json(user);
+
+    } catch (error) {
+         return res.status(500).json({message: error})
+    }
+}
